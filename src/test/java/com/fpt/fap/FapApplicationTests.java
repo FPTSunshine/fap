03d4af9a6137ae7b8149d787c03910ffab68d281
hoangnm <hoangnm00@ringme.vn>
2023-10-12 00:18:18 +0700

    1st commit

---
+++ src/test/java/com/fpt/fap/FapApplicationTests.java
@@ -0,0 +1,22 @@
+package com.fpt.fap;
+
+
+//  class test Spring boot
+// testcase1 : check đăng nhập có đúng username password
+// testcase2 : check permission của role đăng nhập
+// testcase3 : check role đăng nhập
+// testcase4 : check action 404. 400. 500
+// testcase5 :
+
+
+//import org.junit.jupiter.api.Test;
+//import org.springframework.boot.test.context.SpringBootTest;
+//
+//@SpringBootTest
+//class FapApplicationTests {
+////
+////	@Test
+////	void contextLoads() {
+////	}
+////
+//}
